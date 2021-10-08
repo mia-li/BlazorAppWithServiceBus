353fd95ba8b86cd667e857742b8c26e781df2b9b
mia-li <1007473330@qq.com>
2021-10-08 15:58:59 +0800

    blazorapp with servicebus

---
+++ SBReceiver/Program.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Azure.ServiceBus;
+using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using SBShared.Models;
+using System.Text.Json;
+
+namespace SBReceiver
+{
+    class Program
+    {
+        const string connectString = "Endpoint=sb://servicebusz.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=9mc2lhjeX/JBUDGp2BEKraoXZLGzWoWswDv6M1h12I4=";
+        const string queueName = "personqueue";
+        static IQueueClient queueClient;
+
+
+        static async Task Main(string[] args)
+        {
+            queueClient = new QueueClient(connectString, queueName);
+
+            var messageHandlerOptions = new MessageHandlerOptions(ExceptionreceivedHandler)
+            {
+                MaxConcurrentCalls = 1,
+                AutoComplete = false
+            };
+
+            //当queue中来消息了，会call 这个方法：ProcessMessageAsync
+            queueClient.RegisterMessageHandler(ProcessMessageAsync, messageHandlerOptions);
+
+            Console.ReadLine();
+            await queueClient.CloseAsync();
+        }
+
+        private static Task ExceptionreceivedHandler(ExceptionReceivedEventArgs arg)
+        {
+            Console.WriteLine($"Message handler exception:{arg.Exception}");
+            return Task.CompletedTask;
+        }
+
+        private static async Task ProcessMessageAsync(Message message, CancellationToken token)
+        {
+            var jsonString = Encoding.UTF8.GetString(message.Body);
+            PersonModel person = JsonSerializer.Deserialize<PersonModel>(jsonString);
+            Console.WriteLine($"Person Received:{person.FirstName}{person.LastName}");
+
+            //remove the message from the queue
+            await queueClient.CompleteAsync(message.SystemProperties.LockToken);
+        }
+
+        
+    }
+}
---
+++ SBShared/Models/PersonModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace SBShared.Models
+{
+    public class PersonModel
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        
+    }
+}
